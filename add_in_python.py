b7f36be095239fd8dee4fd4731853cc49c78dae4
Ankita3503 <94631478+Ankita3503@users.noreply.github.com>
2021-12-18 22:55:26 +0530

    Add files via upload

---
+++ add_in_python.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Ankita kumari
+#
+# Created:     18-12-2021
+# Copyright:   (c) Ankita kumari 2021
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#add two number
+print("Enter the first number")
+a=input()
+print("Enter the second number")
+b=input()
+sum= int(a)+int(b)
+print("sum of two number=",sum)
