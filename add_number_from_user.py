8fd5a0867752698482a428eb01bc20e5de8ea4dd
Ankita3503 <94631478+Ankita3503@users.noreply.github.com>
2022-01-01 22:48:58 +0530

    Add files via upload

---
+++ add_number_from_user.py
@@ -0,0 +1,10 @@
+n1=int(input("Enter the first number "))
+n2=int(input("Enter the second number "))
+sum=n1+n2
+print("Sum of ",n1, "and", n2 ,"=",sum)
+
+# sum using another method 
+n1=float(input("Enter the first number "))
+n2=float(input("Enter the second number "))
+sum=n1+n2
+print("sum of  two number is " ,sum)
